53a8399601614e507d255110f53791285045e94e
Ridvan Kesken <kesken.dev@gmail.com>
2024-01-11 22:03:07 +0300

    WebAPI layer started to be coded.

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+	private readonly IProductService _productService;
+
+	public ProductsController(IProductService productService)
+	{
+		_productService = productService;
+	}
+	[HttpGet("getall")]
+	public IActionResult GetAll()
+	{
+		var result = _productService.GetAll();
+
+		if (result.Success)
+			return Ok(result.Data);
+
+		return BadRequest(result.Message);
+	}
+}
